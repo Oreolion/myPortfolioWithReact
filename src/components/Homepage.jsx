568f43c1a5c350500f35ad2fc193ea5c4eefb796
oreolion <remyoreo11@gmail.com>
2024-04-14 02:12:42 -0700

    add: minor fixes;

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,10 @@
 import "animate.css";
 import { TypeAnimation } from "react-type-animation";
 import picture from "../assets/images/remyr-removebg-preview.png";
+import { useState } from "react";
 
 const Homepage = () => {
+  const [textColor, setTextColor] = useState("");
   return (
     <>
       <section id="home" className="homepage">
@@ -10,26 +12,44 @@ const Homepage = () => {
           <h5>Hello World...</h5>
           <h1>
             I AM <br />
-            <TypeAnimation
-              sequence={["", 500, "REMY ADEDEJI", 2000]}
-              style={{ fontSize: "3rem" }}
-              speed={10}
-              repeat={2}
-            />
+            <div
+              style={{
+                backgroundColor: textColor,
+              }}
+            >
+              <TypeAnimation
+                sequence={[
+                  "",
+                  1000,
+                  () => setTextColor("rgb(136, 197, 31)"),
+
+                  "REMY ADEDEJI",
+                  2000,
+
+                ]}
+                style={{ fontSize: "3rem", color:  "#ddd",  }}
+                speed={10}
+                repeat={2}
+              />
+            </div>
           </h1>
-          <h3 className="">
+          <h3 className=""
+        >
             <TypeAnimation
               preRenderFirstString={true}
               sequence={[
                 500,
                 "I am a front-End developer", // initially rendered starting point
+
                 1000,
                 "I am a React developer",
+
                 1000,
                 "I am a Vue developer",
                 1000,
                 "I am a Javascript developer",
                 500,
+
               ]}
               speed={30}
               style={{ fontSize: "1.5rem" }}
