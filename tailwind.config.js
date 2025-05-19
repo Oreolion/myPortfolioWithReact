3826431cf1cbdb6163f66cd9530bf0b8895c9a10
REMY ADEDEJI  #RA <remyoreo11@gmail.com>
2025-05-19 07:12:06 -0700

    Added tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(13, 108, 60)',
+        secondary: 'rgb(136, 197, 31)',
+        background: 'rgb(230, 252, 230)'
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+      animation: {
+        'slide-in': 'slideIn 0.5s ease-out',
+        'fade-in': 'fadeIn 0.5s ease-out',
+      },
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
